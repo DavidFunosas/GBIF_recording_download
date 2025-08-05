42d76f3b8eb55a0a2a28bdf39faa503bf587c9a2
David <davidfunosas@gmail.com>
2025-08-05 15:16:49 +0200

    Updated GBIF results

--- R scripts/Downloading and file renaming.R
+++ R scripts/Downloading and file renaming.R
@@ -7,7 +7,7 @@ library(noaa)
 input_data_path <- "../Data/Input/"
 output_data_path <- "../Data/Output/"
 recording_storage_path <- "../Recordings"
-GBIF_dataset_name <- "GBIF_ort_hem_recordings_in_Europe_2025-02-05"
+GBIF_dataset_name <- "GBIF_ort_hem_recordings_in_Europe_2025-08-05"
 
 # Setting a 2-minute timeout to allow enough time to download heavy recordings 
 options(timeout = 120)
